419167e505d33355388a83257666d766fe25ee89
valerieo <valerieouellette@icloud.com>
2022-10-21 14:53:41 -0400

    fonction tour et jouer

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const colonne = 7;
 const rangee = 6;
+const couleur1 = 'red';
+const couleur2 = 'yellow';
+var nbTours = 0;
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
-function initialiser_board(colonne, rangee, couleur) {
+function initialiser_board() {
+    var nbTours = 0;
     let board = document.querySelector("#board")
     board.innerHTML = ""
     board.setAttribute('class', 'board')
     for (let i = 0; i < rangee; i ++) {
         const tr = document.createElement("tr");
-        const divTr = document.createElement("div");
-        divTr.setAttribute('class', 'rangeBoard');
-        tr.append(divTr);
+        tr.setAttribute('class', 'rangeBoard');
         board.append(tr);
         for (let j = 0; j < colonne; j++) {
             const td = document.createElement("td");
@@ -21,11 +23,10 @@ function initialiser_board(colonne, rangee, couleur) {
             tr.append(td);
         }
     }
-    topJetons(colonne, couleur);
-
+    game();
 }
 
-function topJetons(colonne, couleur) {
+function topJetons(couleur) {
     let topBoard = document.querySelector("#topBoard")
     topBoard.setAttribute('class', 'topBoard')
     topBoard.innerHTML = ""
@@ -36,14 +37,39 @@ function topJetons(colonne, couleur) {
     topBoard.append(tr);
     for (let i = 0; i < colonne; i++) {
         const td = document.createElement("td");
-        const boutonJeton = document.createElement("input");
-        boutonJeton.setAttribute('type', 'submit');
+        const boutonJeton = document.createElement("button");
         boutonJeton.setAttribute('class', couleur);
         boutonJeton.setAttribute('id', `btn${i}`);
         td.append(boutonJeton);
         tr.append(td);
     }
 }
+
+function tour() {
+    nbTours += 1;
+    if (nbTours % 2 === 0) {
+        var couleur = couleur1;
+    }
+    else {
+        var couleur = couleur2;
+    }
+    topJetons(couleur)
+    let btn0 = document.querySelector('#btn0');
+    let btn1 = document.querySelector('#btn1');
+    let btn2 = document.querySelector('#btn2');
+    let btn3 = document.querySelector('#btn3');
+    let btn4 = document.querySelector('#btn4');
+    let btn5 = document.querySelector('#btn5');
+    let btn6 = document.querySelector('#btn6');
+    
+    btn0.addEventListener("click", function(){jouer(0, couleur)}, false);
+    btn1.addEventListener("click", function(){jouer(1, couleur)}, false);
+    btn2.addEventListener("click", function(){jouer(2, couleur)}, false);
+    btn3.addEventListener("click", function(){jouer(3, couleur)}, false);
+    btn4.addEventListener("click", function(){jouer(4, couleur)}, false);
+    btn5.addEventListener("click", function(){jouer(5, couleur)}, false);
+    btn6.addEventListener("click", function(){jouer(6, couleur)}, false);}
+
 
 async function jouer(colonne, couleur) {
     for (let i = 0; i < rangee; i++) {
@@ -59,31 +85,17 @@ async function jouer(colonne, couleur) {
     }
 }
 
+function game() {
+    for (i = 0; i < 10; i++) {
+        tour()
+    }
+}
+
 let boutonInit = document.querySelector("#boutonInit");
-boutonInit.addEventListener("click", function(){initialiser_board(colonne, rangee, 'red')}, false);
-
-function tour(colonne, couleur) {
-topJetons(colonne, couleur);
-let btn0 = document.querySelector('#btn0');
-let btn1 = document.querySelector('#btn1');
-let btn2 = document.querySelector('#btn2');
-let btn3 = document.querySelector('#btn3');
-let btn4 = document.querySelector('#btn4');
-let btn5 = document.querySelector('#btn5');
-let btn6 = document.querySelector('#btn6');
-
-btn0.addEventListener("click", function(){jouer(0, couleur)}, false);
-btn1.addEventListener("click", function(){jouer(1, couleur)}, false);
-btn2.addEventListener("click", function(){jouer(2, couleur)}, false);
-btn3.addEventListener("click", function(){jouer(3, couleur)}, false);
-btn4.addEventListener("click", function(){jouer(4, couleur)}, false);
-btn5.addEventListener("click", function(){jouer(5, couleur)}, false);
-btn6.addEventListener("click", function(){jouer(6, couleur)}, false);}
+boutonInit.addEventListener("click", function(){initialiser_board(colonne, rangee, 'yellow')}, false);
 
 
-tour(colonne, 'red');
 
-let boutonJouer = document.querySelector('#boutonJouer');
-boutonJouer.addEventListener("click", function(){jouer(5, 'red')}, false);
+
 
 
