ce90b8270a8d31adaaf5c7b3369e8a188e8513d4
ChristopherDesrosiersMondor <desrosch@gmail.com>
2022-10-21 15:06:19 -0400

    try in ajax

--- projet_final/connect_four/urls.py
+++ projet_final/connect_four/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from connect_four import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('test/', views.request_access , name='request_access'),
 ]
--- projet_final/connect_four/views.py
+++ projet_final/connect_four/views.py
@@ -1,7 +1,17 @@
+import json
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return render(
+        request,
+        'connect_four/base.html'
+    )
+
+def request_access(request):
+    print("DJANGO VIEW")
+    a = request.POST.get('request_data')
+    print(a)
+    return HttpResponse(json.dumps(a),content_type="application/json")
