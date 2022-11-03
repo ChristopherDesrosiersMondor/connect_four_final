88e3614460597aad393beef86768be1bc4515130
ChristopherDesrosiersMondor <desrosch@gmail.com>
2022-11-03 19:03:12 -0400

    test pour deploiement python anywhere
    http://chriswhocodes.pythonanywhere.com/jeu/

--- projet_final/projet_final/settings.py
+++ projet_final/projet_final/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-32u*@^v2fbumkq3m(d709bkgb515@sa_tynk%=x&$-a3q^t_co'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
