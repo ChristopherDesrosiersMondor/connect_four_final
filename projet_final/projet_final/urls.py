f75b43960254945e129fcdde0a1d8ad6a3ecfa79
ChristopherDesrosiersMondor <desrosch@gmail.com>
2023-03-07 15:28:41 -0500

    Update urls.py

--- projet_final/projet_final/urls.py
+++ projet_final/projet_final/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
 
 from projet_final.projet_final.settings import STATIC_ROOT
 
@@ -23,6 +22,3 @@ urlpatterns = [
     path('', include("connect_four.urls")),
     path('admin/', admin.site.urls),
 ]
-
-if settings.DEBUG:
-    urlpatterns += STATIC_ROOT(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
